6e0e77759760e4911894b62af114cfef0f2d8d93
Jayne Hamilton <jayne@Jaynes-MacBook-Pro.local>
2022-10-04 13:19:06 +1300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+  <title>Swapsies</title>
+  <meta name="description" content="House swap app" />
+  <link rel="icon" href="/favicon.ico" />
+  </Head>
+  <Component {...pageProps} />
+  <footer className={styles.footer}>
+       
+  </footer>
+  </>
 }
 
 export default MyApp
---
+++ pages/listings.tsx
@@ -0,0 +1,5 @@
+function Listings() {
+  return <h1>Listings page</h1>
+}
+
+export default Listings
