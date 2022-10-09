d68d2ea25325a1aeefae6e685af3075c7c56564d
Jayne Hamilton <jayneehamilton@gmail.com>
2022-10-09 19:58:16 +1300

    listing id individual page set up

--- pages/listings/index.tsx
+++ pages/listings/index.tsx
@@ -1,18 +1,19 @@
 import { Listing, Image as DbImage, PrismaClient } from '@prisma/client';
 import Link from 'next/link';
 import Image from 'next/image'
-interface ListingProps {
+interface ListingsProps {
   listings: (Listing & { images: DbImage[] })[];
 }
 
-function Listings({ listings }: ListingProps) {
+function Listings({ listings }: ListingsProps) {
   return (
     <>
-      <h1>Listings page</h1>
+      <h1 className="listings-title" >Listings page</h1>
       <ul>
         {listings.map((listing) => (
           <section className="ind-listing" key={listing.id}>
             <h3>{listing.name}</h3>
+            <h4>{listing.location}</h4>
             <Link href={`/listings/${listing.id}`}>click for more</Link>
             <Image className="listing-img" src={`/${listing.images[0].path}`} alt="me" width="550" height="400" />
 
