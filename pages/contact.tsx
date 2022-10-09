a7967dfd0e89f01de29e3c6d0148084e5e08c368
Jayne Hamilton <jayneehamilton@gmail.com>
2022-10-10 09:49:25 +1300

    Form fields added

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,14 +5,20 @@ import type {NextPage} from 'next'
 const Form: NextPage = () => {
   return <form action="/api/form" method="POST">
       <div>
+          <label htmlFor="first-name">First Name</label>
+          <input type="first-name" autoComplete="first-name" name="first-name" required={true}/>
+          <label htmlFor="last-name">Last Name</label>
+          <input type="last-name" autoComplete="last-name" name="last-name" required={true}/>
           <label htmlFor="email">Email</label>
           <input type="email" autoComplete="email" name="email" required={true}/>
+          <label htmlFor="Comment">Comment</label>
+          <input type="comment" autoComplete="comment" name="comment" required={true}/>
       </div>
       <button>Submit</button>
   </form>
-
-  
 }
+
+
 
 
 export default Form
